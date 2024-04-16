e4474e2d3369d8cf272c172e7db4417a78cea92e
Iffy Eleweke <omaleweks@gmail.com>
2024-04-16 13:10:37 -0400

    Added a simple styled component.

--- Day 1/m6-w7-d1-homework/theme-app/src/App.js
+++ Day 1/m6-w7-d1-homework/theme-app/src/App.js
@@ -1,4 +1,39 @@
 import React from "react";
 import styled from "styled-components";
 
-const 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  font-family: sans-serif;
+`;
+
+const SwitchTheme = styled.div`
+  display: flex;
+  height: 75px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px;
+`;
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <SwitchTheme>
+        <h1>Theme App</h1>
+        <p>
+          <input type="checkbox" /> 
+        </p>
+      </SwitchTheme>
+      <Container>
+        <h2>Welcome!</h2>
+        <h3>Full Stack Web Development</h3>
+      </Container>
+    </React.Fragment>
+  )
+};
+
+export default App;
